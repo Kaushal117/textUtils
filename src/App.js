faf06cd063d6e125b7cbb3cbbed814c16d31dd6d
Kaushal117 <hodilsinghjeemain@gmail.com>
2022-06-27 15:43:50 +0530

    dark mode enabled

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-// import React from 'react'
-import React from 'react'
-
 import './App.css';
-// import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/textForm';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
+ 
 function App() {
-  return (
-    <React.Fragment>
-    
-<Navbar title="algoWorks"  aboutText="KAuSHAL KISHOR"/>
-<div className="container my-3" >
-  <TextForm heading="enter the text to analyze" />
-</div>
-{/* <About/> */}
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
 
-    </React.Fragment>
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  return (
+    <>
+   
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </> 
   );
 }
 
