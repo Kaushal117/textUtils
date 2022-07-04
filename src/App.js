d03dbc8a89d0a9ed4ede5388e2db1cec08f03d81
Kaushal117 <hodilsinghjeemain@gmail.com>
2022-07-04 12:20:54 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-
- 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+ //press ctrl -----to show data in that particular page
 function App() {
   const [mod, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
@@ -37,15 +46,44 @@ function App() {
     }
   }
   return (
-    <>
+    <React.Fragment>
+
+<Router>
+    <div className="site-container">
+         <Navbar title="TextUtils" mode={mod} toggleMode={toggleMod} />
+                 <Routes>
+                    
+                    <Route   path="/about" element={<About />} />
+                    <Route exact  path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mod} />} />
+                 
+               
+                </Routes> 
+             {/* <Routes>
+
+                <Route exact path="/about">
+                  <About/>
+                </Route>
+
+                <Route exact path="/">
+                  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mod}/>
+                </Route>
+
+             </Routes> */}
+           
+    </div> 
+        
+</Router>
+
+
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mod} toggleMode={toggleMod} />
+    
+    {/* <Navbar title="TextUtils" mode={mod} toggleMode={toggleMod} />
     <Alert alert={alert}/>
     <div className="container my-3">
     <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mod}/>
-    </div>
-    </> 
+    </div> */}
+    </React.Fragment> 
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,6 +44,7 @@
 // };
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
@@ -56,10 +57,12 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            {/* <a className="nav-link active" aria-current="page" href="/">Home</a> */}
+                            <Link to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.aboutText}</a>
+                            {/* <a className="nav-link" href="/">{props.aboutText}</a> */}
+                            <Link to="/about">About</Link>
                         </li>
                     </ul>
                     {/* <form className="d-flex">
